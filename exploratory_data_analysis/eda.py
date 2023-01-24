7c54c1385fa1cdac4d33bcb6e0296930c4e29167
cristian-castro-a <cr.castro.alvarez@gmail.com>
2023-01-24 21:11:01 +0100

    2023-01-24 Update

--- exploratory_data_analysis/eda.py
+++ exploratory_data_analysis/eda.py
@@ -3,7 +3,7 @@ import logging
 import hydra
 from omegaconf import DictConfig
 
-from utils.exploratory_data_analysis import get_eda_summary, get_predictor_names, plot_time_series
+from utils.exploratory_data_analysis import get_eda_summary, plot_time_series
 from utils.parsers import read_data
 from utils.preprocessing import preprocess_data
 from utils.sdk_config import SDKConfig
@@ -17,13 +17,15 @@ def main(config: DictConfig) -> None:
     """
     Exploratory data analysis
     """
+    logger.info('Setting working directories for the project')
+    SDKConfig().set_working_dirs()
+
     data_dict = read_data(data_dir=SDKConfig().data_dir, config=config)
     processed_data_dict = preprocess_data(data_dict=data_dict)
 
-    # Visualization of predictors
+    # Visualization of predictors and summary of statistics
     path_to_results = SDKConfig().get_output_dir("plots_predictors")
     plot_time_series(processed_data_dict, path_to_results)
-
     eda_summary = get_eda_summary(processed_data_dict=processed_data_dict)
 
 
